a33cd1d68d0bc75928ca69aa0371ec3cf8bf268f
unknown <jayavardhan.pappala@gmail.com>
2026-04-15 22:43:37 +0530

    fix limiter init order

--- app.py
+++ app.py
@@ -8,10 +8,15 @@ from tasks import build_video
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
-limiter = Limiter(get_remote_address, app=app)  
 import os
 
 app = Flask(__name__)
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    app=app,
+    default_limits=["100 per day"]
+) 
 
 init_db()
 
